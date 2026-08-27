d751285e4030382a28db70adf1f447733f7fb9f6
AD MIN <AD MIN@LAPTOP-LQ9638Q2>
2026-08-27 15:57:14 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Nhập số thứ nhất (a): ");
+        double a = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        Console.Write("Nhập số thứ hai (b): ");
+        double b = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        Console.Write("Nhập toán tử (+,-,*,/,%): ");
+        char c = Console.ReadLine()![0];
+
+        string result = (c, b) switch {
+
+            ('+', _) => (a + b).ToString("F2", CultureInfo.InvariantCulture),
+            ('-', _) => (a - b).ToString("F2", CultureInfo.InvariantCulture),
+            ('/' or '%', 0) => "không thể chia cho 0",
+            ('/', _) => (a / b).ToString("F2", CultureInfo.InvariantCulture),
+            ('%', _) => (a % b).ToString("F2", CultureInfo.InvariantCulture),
+            _ => "Lỗi: Phép toán không hợp lệ!"};
+        Console.WriteLine($"{result}");
+        Console.ReadKey();
+    }
+}
